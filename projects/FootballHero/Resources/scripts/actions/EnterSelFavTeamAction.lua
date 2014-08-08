9951708fdcadea8dbc38870f7f1c9deb990f4032
Yu Zheng(Sam) <sam@tradehero.mobi>
2014-08-08 12:49:11 +0800

    Clean up unused png and UI.

--- projects/FootballHero/Resources/scripts/actions/EnterSelFavTeamAction.lua
+++ projects/FootballHero/Resources/scripts/actions/EnterSelFavTeamAction.lua
@@ -5,11 +5,14 @@ local Event = require("scripts.events.Event").EventList
 local Logic = require("scripts.Logic").getInstance()
 
 function action( param )
+	EventManager:postEvent( Event.Enter_Match_List )
 
+	--[[
 	if Logic:getStartLeagueId() == 0 then
 		local selFavTeamScene = require("scripts.views.SelFavTeamScene")
     	selFavTeamScene.loadFrame()
 	else
 	    EventManager:postEvent( Event.Enter_Match_List )
 	end
+	--]]
 end
