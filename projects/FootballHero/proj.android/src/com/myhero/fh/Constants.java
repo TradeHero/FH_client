6f1ca5c40b42043d6db815514444307e968c0dbf
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-08-18 12:39:17 +0800

    Update localytics apikey

--- projects/FootballHero/proj.android/src/com/myhero/fh/Constants.java
+++ projects/FootballHero/proj.android/src/com/myhero/fh/Constants.java
@@ -2,5 +2,5 @@ package com.myhero.fh;
 
 public class Constants {
   public static final boolean RELEASE = false;
-  public static final String LOCALYTICS_APP_KEY = "TODO"; // TODO
+  public static final String LOCALYTICS_APP_KEY = "2257be891766219b898e1dd-05dbeb7a-2691-11e4-4a80-00a426b17dd8";
 }
