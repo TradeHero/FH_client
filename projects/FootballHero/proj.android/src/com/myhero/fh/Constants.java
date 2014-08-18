eee41f73506419dfce99fc200dedc4b121ac2150
Yu Zheng(Sam) <sam@tradehero.mobi>
2014-08-18 17:22:30 +0800

    Change the API key for android to use the same one as the IOS.

--- projects/FootballHero/proj.android/src/com/myhero/fh/Constants.java
+++ projects/FootballHero/proj.android/src/com/myhero/fh/Constants.java
@@ -2,5 +2,5 @@ package com.myhero.fh;
 
 public class Constants {
   public static final boolean RELEASE = false;
-  public static final String LOCALYTICS_APP_KEY = "2257be891766219b898e1dd-05dbeb7a-2691-11e4-4a80-00a426b17dd8";
+  public static final String LOCALYTICS_APP_KEY = "d16d149eabf971a5b376a43-aa0e6fc0-1c50-11e4-49cb-00a426b17dd8";
 }
