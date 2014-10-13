7ac6a5d19f33c405e749d3cbe1dbf70b508d95c6
Vincent Tee <vincent@tradehero.mobi>
2014-10-13 11:39:15 +0800

    do not sort popular matches by time within the same date

--- projects/FootballHero/Resources/scripts/actions/EnterMatchListAction.lua
+++ projects/FootballHero/Resources/scripts/actions/EnterMatchListAction.lua
@@ -13,7 +13,7 @@ local mCheckShowMarketingMessage
 
 function action( param )
     local leagueId = Logic:getStartLeagueId()
-    if Logic:getPreviousLeagueSelected() > 0 then
+    if Logic:getPreviousLeagueSelected() ~= 0 then
         leagueId = Logic:getPreviousLeagueSelected()
     end
 
@@ -68,30 +68,32 @@ end
 function onRequestSuccess( matchList )
     local MatchListData = require("scripts.data.MatchListData").MatchListData
 
-    -- Sort the match according to its start time.
-    local currentTime = os.time()
-    local currentDate = os.time{year=os.date("%Y", currentTime), month=os.date("%m", currentTime), day=os.date("%d", currentTime), hour=0}
-    table.sort( matchList, function ( n1, n2 )
-        if n1["StartTime"] < n2["StartTime"] then
-            if n2["StartTime"] < currentDate then
-                return false
-            elseif n1["StartTime"] < currentDate then
-                return false
+    if mLeagueId ~= Constants.MOST_POPULAR_LEAGUE_ID then
+        -- Sort the match according to its start time.
+        local currentTime = os.time()
+        local currentDate = os.time{year=os.date("%Y", currentTime), month=os.date("%m", currentTime), day=os.date("%d", currentTime), hour=0}
+        table.sort( matchList, function ( n1, n2 )
+            if n1["StartTime"] < n2["StartTime"] then
+                if n2["StartTime"] < currentDate then
+                    return false
+                elseif n1["StartTime"] < currentDate then
+                    return false
+                else
+                    return true
+                end
+            elseif  n1["StartTime"] > n2["StartTime"] then
+                if n1["StartTime"] < currentDate then
+                    return true
+                elseif n2["StartTime"] < currentDate then
+                    return true
+                else
+                    return false
+                end
             else
-                return true
-            end
-        elseif  n1["StartTime"] > n2["StartTime"] then
-            if n1["StartTime"] < currentDate then
-                return true
-            elseif n2["StartTime"] < currentDate then
-                return true
-            else
                 return false
             end
-        else
-            return false
-        end
-    end )
+        end )
+    end
 
     -- Group and sort.
     local sortedMatchList = MatchListData:new()
