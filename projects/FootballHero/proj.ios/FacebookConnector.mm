0bab84997f1b63b9b65f6112429ae3d29950b142
Yu Zheng(Sam) <sam@tradehero.mobi>
2014-10-16 17:45:22 +0800

    Add the share code.

--- projects/FootballHero/proj.ios/FacebookConnector.mm
+++ projects/FootballHero/proj.ios/FacebookConnector.mm
@@ -37,7 +37,76 @@ void FacebookConnector::initSession()
 
 void FacebookConnector::login()
 {
+    /*
+    FBSession* session = [FBSessionSingleton sharedInstance].session;
+    if (session.state != FBSessionStateCreated) {
+        // Create a new, logged out session.
+        session = [[FBSession alloc] init];
+    }
     
+    [FBSession openActiveSessionWithReadPermissions: @[@"public_profile", @"user_friends", @"email", @"user_birthday"] allowLoginUI:YES completionHandler:^(FBSession *aSession, FBSessionState status, NSError *error) {
+        
+        [FBSessionSingleton sharedInstance].session = aSession;
+        const char* accessToken =[aSession.accessTokenData.accessToken UTF8String];
+        Social::FacebookDelegate::sharedDelegate()->accessTokenUpdate(accessToken);
+    }];
+    */
+    
+    id<ISSAuthOptions> authOptions = [ShareSDK authOptionsWithAutoAuth:YES
+                                                         allowCallback:YES
+                                                         authViewStyle:SSAuthViewStyleFullScreenPopup
+                                                          viewDelegate:nil
+                                               authManagerViewDelegate:nil];
+    [authOptions setScopes:[NSDictionary dictionaryWithObjectsAndKeys: @[@"public_profile", @"user_friends", @"email", @"publish_actions"],
+                            [NSNumber numberWithInt:ShareTypeFacebook], nil]];
+    
+    [ShareSDK authWithType:ShareTypeFacebook
+                   options:authOptions                                          //授权选项，包括视图定制，自动授权
+                    result:^(SSAuthState state, id<ICMErrorInfo> error) {
+                        if (state == SSAuthStateSuccess)
+                        {
+                            id<ISSPlatformCredential> credential = [ShareSDK getCredentialWithType:ShareTypeFacebook];
+                            NSLog(@"成功 %@ ", [credential token]);
+                            const char* accessToken =[[credential token] UTF8String];
+                            Social::FacebookDelegate::sharedDelegate()->accessTokenUpdate(accessToken);
+                            share("Match share", "Content");
+                        }
+                        else if (state == SSAuthStateFail or state == SSAuthStateCancel)
+                        {
+                            NSLog(@"失败");
+                            Social::FacebookDelegate::sharedDelegate()->accessTokenUpdate(nil);
+                        }
+                    }];
+    
+}
+
+void FacebookConnector::share(const char* title, const char* content)
+{
+    //构造分享内容
+    id<ISSContent> publishContent = [ShareSDK content:[NSString stringWithUTF8String:title]
+                                       defaultContent:[NSString stringWithUTF8String:content]
+                                                image:nil
+                                                title:@"ShareSDK"
+                                                  url:@"http://footballheroapp.com"
+                                          description:@"Share"
+                                            mediaType:SSPublishContentMediaTypeNews];
+    
+    [ShareSDK showShareActionSheet:nil
+                         shareList:nil
+                           content:publishContent
+                     statusBarTips:YES
+                       authOptions:nil
+                      shareOptions: nil
+                            result:^(ShareType type, SSResponseState state, id<ISSPlatformShareInfo> statusInfo, id<ICMErrorInfo> error, BOOL end) {
+                                if (state == SSResponseStateSuccess)
+                                {
+                                    NSLog(@"分享成功");
+                                }
+                                else if (state == SSResponseStateFail)
+                                {
+                                    NSLog(NSLocalizedString(@"TEXT_SHARE_FAI", @"发布失败!error code == %d, error code == %@"), [error errorCode], [error errorDescription]);
+                                }
+                            }];
 }
 
 void FacebookConnector::grantPublishPermission(const char* permission)
