005861661b29ee4be68c8d82811eb4d29ae5c015
Yu Zheng(Sam) <sam@tradehero.mobi>
2015-04-03 14:09:13 +0800

    Change to use Loggly.

--- projects/FootballHero/Resources/scripts/actions/DoLogReport.lua
+++ projects/FootballHero/Resources/scripts/actions/DoLogReport.lua
@@ -2,11 +2,16 @@ module(..., package.seeall)
 
 local Constants = require("scripts.Constants")
 local RequestUtils = require("scripts.RequestUtils")
+local Json = require("json")
 
 
 local PROJECT_ID = "544021a2e18b11e3923422000ab5bb50"
 local ACCOUNT_ID = "sXq6y8k3RoWKE8pyCSEJfZH9LsTc3MmCv9wjNMVjx3zhSFZN-H9Vu0_VveSM2ITwFMxJF5nYlD4="
 local SPLUNK_LOG_IP = "https://api.p3js-eqtr.data.splunkstorm.com/1/inputs/http"
+
+local LOGGLY_IP = "http://logs-01.loggly.com/inputs/f5c98c57-6d4d-4e6d-9ccb-1fd03e899e70/tag/http/"
+local KEY_OF_VERSION = "current-version-code"
+
 
 function reportConfigError( log )
     if not RequestUtils.USE_DEV then
@@ -27,17 +32,51 @@ function reportError( log )
 end
 
 function report( log, sourceType )
-	local handler = function( isSucceed, body, header, status, errorBuffer )
+    --[[
+        --Splunk storm log
+        local handler = function( isSucceed, body, header, status, errorBuffer )
+        end
+
+        local httpRequest = HttpRequestForLua:create( CCHttpRequest.kHttpPost )
+        httpRequest:addHeader( Constants.CONTENT_TYPE_PLAINTEXT )
+        httpRequest:setUserpwd( "samyu:"..ACCOUNT_ID )
+        httpRequest:getRequest():setRequestData( log, string.len( log ) )
+
+        local url = SPLUNK_LOG_IP
+        url = url.."?index="..PROJECT_ID
+        url = url.."&sourcetype="..sourceType
+
+        httpRequest:sendHttpRequest( url, handler )
+    ]]
+
+    --Loggly log
+    local requestObject = {}
+    requestObject["ErrorType"] = sourceType
+    requestObject["ClientVersion"] = CCUserDefault:sharedUserDefault():getStringForKey( KEY_OF_VERSION )
+    requestObject["Device"] = getDeviceName()
+    requestObject["StackTrace"] = log
+    local requestContent = Json.encode( requestObject )
+
+    local handler = function( isSucceed, body, header, status, errorBuffer )
     end
 
-	local httpRequest = HttpRequestForLua:create( CCHttpRequest.kHttpPost )
+    local httpRequest = HttpRequestForLua:create( CCHttpRequest.kHttpPost )
     httpRequest:addHeader( Constants.CONTENT_TYPE_PLAINTEXT )
-    httpRequest:setUserpwd( "samyu:"..ACCOUNT_ID )
-    httpRequest:getRequest():setRequestData( log, string.len( log ) )
+    httpRequest:getRequest():setRequestData( requestContent, string.len( requestContent ) )
 
-    local url = SPLUNK_LOG_IP
-    url = url.."?index="..PROJECT_ID
-    url = url.."&sourcetype="..sourceType
+    local url = LOGGLY_IP
 
     httpRequest:sendHttpRequest( url, handler )
+end
+
+function getDeviceName()
+    if CCApplication:sharedApplication():getTargetPlatform() == kTargetAndroid then
+        return "Android"
+    elseif CCApplication:sharedApplication():getTargetPlatform() == kTargetIphone then
+        return "Iphone"
+    elseif CCApplication:sharedApplication():getTargetPlatform() == kTargetIpad then
+        return "Ipad"
+    else
+        return "Unknown"
+    end
 end
