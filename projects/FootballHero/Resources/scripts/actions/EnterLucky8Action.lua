8bc97cc159ce4c8a0edab9ed07069612c842aa41
andy <andy@andydeMacBook-Pro.local>
2015-04-24 18:27:52 +0800

    add http request of lucky8 games

--- projects/FootballHero/Resources/scripts/actions/EnterLucky8Action.lua
+++ projects/FootballHero/Resources/scripts/actions/EnterLucky8Action.lua
@@ -13,5 +13,26 @@ function action( param )
     if lucky8Scene.isFrameShown() ~= true then
         lucky8Scene.loadFrame()
     end
+
+    local url = RequestUtils.GET_LUCKY8_GAMES
+    local requestInfo = {}
+    requestInfo.requestData = ""
+    requestInfo.url = url
+    local handler = function ( isSucceed, body, header, status, errorBuffer )
+    	RequestUtils.messageHandler( requestInfo, isSucceed, body, header, status, errorBuffer, RequestUtils.HTTP_200, true, onRequestSuccess, onRequestFailed )
+    end
+
+    local httpRequest = HttpRequestForLua:create( CCHttpRequest.kHttpGet )
+    httpRequest:addHeader( Logic:getAuthSessionString() )
+    httpRequest:sendHttpRequest( url, handler )
+
+    ConnectingMessage.loadFrame()
 end
 
+function onRequestSuccess( data )
+	
+end
+
+function onRequestFailed( jsonResponse )
+	
+end
