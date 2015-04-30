b767fc6d5ca1285cc1304975a8c7a4a6c5f23fda
andy <forwind2014@gmail.com>
2015-04-30 17:29:31 +0800

    hide live button

--- projects/FootballHero/Resources/scripts/views/MatchListScene.lua
+++ projects/FootballHero/Resources/scripts/views/MatchListScene.lua
@@ -42,7 +42,7 @@ function loadFrame( matchList, leagueKey )
     SceneManager.clearNAddWidget( widget )
     
     Header.loadFrame( widget, nil, false )
-    Header.showLiveButton( true )
+    Header.showLiveButton( false )
 
     Navigator.loadFrame( widget )
     Navigator.chooseNav( 1 )
