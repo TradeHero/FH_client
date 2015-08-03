6d1514110f05153dcfee470a6983d91d1085190b
Yu Zheng(Sam) <sam@tradehero.mobi>
2015-08-03 17:25:36 +0800

    Fix competition chat not working bug.

--- projects/FootballHero/Resources/scripts/views/CompetitionChatScene.lua
+++ projects/FootballHero/Resources/scripts/views/CompetitionChatScene.lua
@@ -42,6 +42,7 @@ function loadFrame( competitionId )
     moreBt:addTouchEventListener( moreEventHandler )
 
     local messageInput = ViewUtils.createTextInput( mWidget:getChildByName( MESSAGE_CONTAINER_NAME ), Constants.String.message_hint, 470, 50 )
+    messageInput:setTouchPriority( SceneManager.TOUCH_PRIORITY_MINUS_ONE )
 
     initTitle();
     doGetLatestMessages()
